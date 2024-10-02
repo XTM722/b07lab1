d467ad1072f44385bde1e1465b8552c579eb81d8
kevin <kevintsinghua@gmail.com>
2024-10-02 18:23:53 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,19 +1,60 @@
-public class Driver {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6, 0, 0, 5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0, -2, 0, 0, -9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s  = p1.add(p2);
-        System.out.println("s(0.1)=" + s.evaluate(0.1));
-        if(s.hasRoot(1)){
-            System.out.println("1 is a root of s");
+public class Driver {
+    public static void main(String[] args) throws IOException{
+        //test 1
+        double [] coefficients1 = {5, -3,  7};
+        int [] exponents1 = {0, 2, 8};
+        Polynomial polynomial1 = new Polynomial(coefficients1, exponents1);
+        System.out.println("First Polynomial:");
+        polynomial1.displayCoefficientsAndExponents();
+
+        //test 2 add method
+
+        double [] coefficients2 = {1, 2, 3};
+        int [] exponents2 = {1, 2, 3};
+
+        double [] coefficients3 = {4, 5, 6};
+        int [] exponents3 = {4, 5, 6};
+
+        Polynomial polynomial2 = new Polynomial(coefficients2, exponents2);
+        System.out.println("Second polynomial:");
+        polynomial2.displayCoefficientsAndExponents();
+        Polynomial polynomial3 = new Polynomial(coefficients3, exponents3);
+        System.out.println("Third polynomial:");
+        polynomial3.displayCoefficientsAndExponents();
+
+        System.out.println("-------TESTING ADD METHOD--------");
+        Polynomial addPolynomial = polynomial2.add(polynomial3);
+        System.out.println("We added the second polynomial and the third polynomial:");
+        addPolynomial.displayCoefficientsAndExponents();
+
+        //test 3 evaluate with a x value
+
+        System.out.println("------TESTING FOR EVALUATE METHOD WITH VALUE X------");
+        double x = 2;
+        double evalueResult = polynomial1.evaluate(x);
+        System.out.println("We are evaluating value x = " + x + ", for the polynomial 1 is: " + evalueResult);
+
+        //test 4 testing the multiply method
+        System.out.println("------TESTING FOR MULTIPLY METHOD WITH POLYNOMIAL 2 AND POLYNOMIAL 3");
+        Polynomial multiplyPolynomial = polynomial2.multiply(polynomial3);
+        multiplyPolynomial.displayCoefficientsAndExponents();
+
+        //test 5, testing for root
+        System.out.println("------TESTING FOR CHECKING ROOTS WITH VALUE X = 2 FOR THE POLYNOMIAL 2------");
+        polynomial2.hasRoot(x);
+        System.out.println("Polynomial 2 has root at x = "+ x + " ," + polynomial2.hasRoot(x));
+
+        //test 6 saving file
+        System.out.println("------TESTING FOR METHOD saveToFile FOR POLYNOMIAL 3");
+        try {
+            polynomial3.saveToFile("output_polynomial3.txt");
+            System.out.println("Polynomial 3 has been saved to output_polynomial3.txt");
+        } catch (Exception e) {
+            System.out.println("Error saving the polynomial: " + e.getMessage());
         }
-        else{
-            System.out.println("1 is not a root of s");
-        }
+
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,43 +1,168 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class Polynomial {
 
     double [] coefficients;
+    int [] exponents;
 
     public Polynomial(){
         this.coefficients = new double[]{0};
+        this.exponents = new int[] {0};
+    }
+    public Polynomial (File file) throws FileNotFoundException{
+        HashMap<Integer, Double> Map = new HashMap<>();
+        Scanner sc = new Scanner(file);
+        String text = sc.nextLine();
+        sc.close();
+
+        String [] terms = text.split("(?=[+-])");
+        for(String term : terms){
+            term = term.trim();
+            if(term.contains("x")){
+                String [] coeff_and_exponent_parts = term.split("x");
+                double coefficient;
+                if(coeff_and_exponent_parts[0].equals("") || coeff_and_exponent_parts[0].equals("+")){
+                    coefficient = 1.0;
+                }
+                else if (coeff_and_exponent_parts[0].equals("-")){
+                    coefficient = -1.0;
+                }else{
+                    coefficient = Double.parseDouble(coeff_and_exponent_parts[0]);
+                }
+                int exponent = (coeff_and_exponent_parts.length > 1) ? Integer.parseInt(coeff_and_exponent_parts[1]) : 1;
+                Map.put(exponent, coefficient);
+            }
+            else{
+                Map.put(0, Double.parseDouble(term));
+            }
+        }
+        convertMaptoArrays(Map);
+    }
+    public void saveToFile(String fileName) throws IOException{
+        FileWriter writer = new FileWriter(fileName);
+        StringBuilder sBuilder = new StringBuilder();
+        for(int i = 0 ; i < this.coefficients.length; i++){
+            double coefficient = this.coefficients[i];
+            int exponent = this.exponents[i];
+            if(coefficient == 0) continue;
+            
+            if(sBuilder.length() > 0 && coefficient > 0){
+                sBuilder.append('+');
+            }
+            if(exponent == 0 || Math.abs(coefficient) !=1) {
+                sBuilder.append(coefficient);
+            }else if(coefficient == -1){
+                sBuilder.append('-');
+            }
+            if(exponent >0){
+                sBuilder.append('x');
+                if(exponent > 1){
+                    sBuilder.append(exponent);
+                }
+            }
+        }
+        writer.write(sBuilder.toString());
+        writer.close();
+    }
+    public void convertMaptoArrays (HashMap<Integer, Double> Map){
+        this.coefficients = new double[Map.size()];
+        this.exponents = new int [Map.size()];
+
+        List<Integer> sortedExponents = new ArrayList<>(Map.keySet());
+        Collections.sort(sortedExponents);
+        int index = 0;
+        for(int exponent: sortedExponents){
+            this.coefficients[index] = Map.get(exponent);
+            this.exponents[index] = exponent;
+            index++;
+        }
     }
 
-    public Polynomial(double [] coefficients){
+    public Polynomial(double [] coefficients, int [] exponents){
         this.coefficients = coefficients;
+        this.exponents = exponents;
 
     }
 
     public Polynomial add(Polynomial p){
-        int size1 = this.coefficients.length;
-        int size2 = p.coefficients.length;
-        int maxSize = Math.max(size1, size2);
-
-        double [] new_coefficients = new double[maxSize];
-        for(int i = 0; i < maxSize; i++){
-            if(i < size1){
-                new_coefficients[i] += this.coefficients[i];
+        HashMap<Integer, Double> Map = new HashMap<>();
+        for(int i = 0; i < this.coefficients.length; i++){
+            int thisExponent = this.exponents[i];
+            double thisCoefficient = this.coefficients[i];
+            if(Map.containsKey(thisExponent)){
+                Map.put(thisExponent, Map.get(thisExponent) + thisCoefficient);
             }
-            if(i < size2){
-                new_coefficients[i] += p.coefficients[i];
+            else{
+                Map.put(thisExponent, thisCoefficient);
             }
         }
-        Polynomial newPolynomial = new Polynomial(new_coefficients);
-        return newPolynomial;
+
+        for(int i = 0; i < p.coefficients.length; i++){
+            int pExponent = p.exponents[i];
+            double pCoefficient = p.coefficients[i];
+            if(Map.containsKey(pExponent)){
+                Map.put(pExponent, Map.get(pExponent) + pCoefficient);
+            }else{
+                Map.put(pExponent, pCoefficient);
+            }
+        }
+        return mapConvertPolynomial(Map);
+    }
+    private Polynomial mapConvertPolynomial(HashMap<Integer, Double> Map){
+        int [] newExponents = new int[Map.size()];
+        double [] newCoefficients = new double[Map.size()];
+        //sort using List
+
+        List<Integer> sortedExponents = new ArrayList<>(Map.keySet());
+        Collections.sort(sortedExponents);
+        int index = 0;
+        for(Integer exponent : sortedExponents){
+            newExponents[index] = exponent;
+            newCoefficients[index] = Map.get(exponent);
+            index++;
+        }
+        return new Polynomial(newCoefficients, newExponents);
+    }
+
+    public Polynomial multiply(Polynomial p){
+        HashMap<Integer, Double> Map = new HashMap<>();
+        for(int i = 0; i < this.coefficients.length; i++){
+            for(int j = 0; j < p.coefficients.length; j++){
+                int newExponent = this.exponents[i] + p.exponents[j];
+                double newCoefficient = this.coefficients[i] + p.coefficients [j];
+                if(Map.containsKey(newExponent)){
+                    Map.put(newExponent, Map.get(newExponent) + newCoefficient);
+                }
+                else{
+                    Map.put(newExponent, newCoefficient);
+                }
+            }
+        }
+        return mapConvertPolynomial(Map);
     }
 
     public double evaluate(double x){
         double sum = 0;
         for(int i = 0; i < this.coefficients.length; i++){
-            sum += this.coefficients[i] * Math.pow(x, i);
+            sum += this.coefficients[i] * Math.pow(x, this.exponents[i]);
         }
         return sum;
     }
 
     public boolean hasRoot(double x){
         return evaluate(x) == 0;
+    }
+    public void displayCoefficientsAndExponents() {
+        System.out.println("Coefficients: " + Arrays.toString(coefficients));
+        System.out.println("Exponents: " + Arrays.toString(exponents));
     }
 }
